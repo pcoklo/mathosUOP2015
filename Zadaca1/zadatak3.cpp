101aacc95d3563e2f8c83fbfd42d546e0d3abe76
pcoklo <petarcoklo@gmail.com>
2015-03-24 13:17:43 +0100

    2,3,4 rjeseni

--- Zadaca1/zadatak3.cpp
+++ Zadaca1/zadatak3.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 void ispis(float *A, int m, int n);
 void copyFromLame2Awsome(float M[], float *A, int m, int n);
+void matrix_multiply(float *A, float *B, float *C, int m, int k, int n);
 
 int main(){
-
-
+	
 	return 0;
 }
 
@@ -22,4 +22,13 @@ void ispis(float *A, int m, int n){
 			cout << A[i*n+j] << "\t";
 	}
 	cout << endl;
+}
+
+void matrix_multiply(float *A, float *B, float *C, int m, int k, int n){
+	for(int i=0; i<m; i++)
+		for(int j=0; j<n; j++){
+			C[i*n+j]=0;
+			for(int l=0; l<k; l++)
+				C[i*n+j]=C[i*n+j]+(A[i*k+l]*B[l*n+j]);
+		}
 }
