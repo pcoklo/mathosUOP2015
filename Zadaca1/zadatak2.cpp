60098c382c1777df4321a9616ba538a7da289eb0
pcoklo <petarcoklo@gmail.com>
2015-03-25 11:09:03 +0100

    dodani komentari
    eo vam pičke!

--- Zadaca1/zadatak2.cpp
+++ Zadaca1/zadatak2.cpp
@@ -6,23 +6,34 @@ void ispis(float *A, int m, int n);
 void copyFromLame2Awsome(float M[], float *A, int m, int n);
 
 int main(){
-	float *A;
-	int m=5, n=4;
+	float *A;		//pointer tipa float zvan A
+	int m=5, n=4;		//timenzije
 
-	float M[]={
-		-1, 2, 3, 0,
-		0, 2, 5, 1,
-		4, 3, 1, 0,
+	float M[]={		//matrica iz koje prekopiramo elemente
+		-1, 2, 3, 0,	//nije potrebna
+		0, 2, 5, 1,	//ali jako
+		4, 3, 1, 0,	//olakšava pisanje
 		1, 2, -5, 7,
 		0, 0, 2, 1
 	};
+	
+	/*** da nema te matrice pisali bi ovako nešto
+	A[0]=-1;	A[1]=2;		A[2]=3;		A[3]=0;
+        A[4]=0;		A[5]=2;		A[6]=5;		A[7]=1;
+        A[8]=4;		A[9]=3;		A[10]=1;	A[11]=0;
+        A[12]=1;	A[13]=2;	A[14]=-5	A[15]=7;
+        A[16]=0;	A[17]=0;	A[18]=2;	A[19]=1;
 
-	A = new float [n*m];
+	/*** i tako svaki element dobije svoju vrijednost
+	 * što nema smisla
+	 * */
 
-	copyFromLame2Awsome(M,A,m,n);
-	ispis(A,m,n);
-	A = (float*) transpose(A,m,n);
-	ispis(A,m,n);
+	A = new float [n*m];		//dinamički alociramo m*n mjesta za matricu
+
+	copyFromLame2Awsome(M,A,m,n);	//kopiramo sve elemente iz M[] matrice u novu *A matricu
+	ispis(A,m,n);			//i ispišemo
+	A = (float*) transpose(A,m,n);	//"A = transponirano(A)" ovaj (float*) nađite sami
+	ispis(A,m,n);			//ispis transponirano A
 
 	return 0;
 }
@@ -30,26 +41,38 @@ int main(){
 void copyFromLame2Awsome(float M[], float *A, int m, int n){
 	for(int i=0; i<m; i++)
 		for(int j=0; j<n; j++)
-			A[i*n+j] = M[i*n+j];
+			A[i*n+j] = M[i*n+j];	//kroz dvije for petlje kopiramo elemente iz M[] u *A
+	
+	// A može i ovako
+//	for(int i=0; i<m*n; i++) A[i] = M[i];
 }
 
 void ispis(float *A, int m, int n){
 	for(int i=0; i<m; i++){
-		for(int j=0; j<n; j++)
+		for(int j=0; j<n; j++)		//kroz dvije for petlje ispisujemo elemente
 			cout << A[i*n+j] << "\t";
+		cout << endl;
 	}
 	cout << endl;
+	// može i ovako
+/*	for(int i=0; i<m*n; i++){
+		cout << A[i] << "\t";
+		if((i+1)%n == 0) cout << endl;
+	}
+	cout << endl;
+/**/
 }
 
 void *transpose(float *A, int &m, int &n){
-	int temp=m;
-	m=n; n=temp;
+	// &m i &n su reference na variable koje prosljeđujemo
+	int y=m;	//pa tako ako ih zamjenimo
+	m=n; n=y;	//u ovoj funkciji zamjenjene su svuda
 
-	float *tmp = new float [m*n];
+	float *B = new float [m*n];	// B je matrica u koju transponiramo matricu A
 	for(int i=0;i<m;i++)
 		for(int j=0;j<n;j++)
-			tmp[i*n+j]=A[j*m+i];
+			B[i*n+j]=A[j*m+i];	//magija!
 
-	A=tmp;
-	return A;
+	A=B;	// A = A transponirano
+	return A;	//vrati A nazad ... A=(float*)transpose(Am,n);
 }
