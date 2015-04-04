719d16b43bb0fe92ead8ea7b46cf3815b93c989c
pcoklo <petarcoklo@gmail.com>
2015-04-04 15:19:59 +0200

    zadatak 3 initial
    lijepo potpisano

---
+++ Zadaca2/Zadatak3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+class Tocka
+{
+public:
+	float x;
+	float y;
+	Tocka(){};
+	Tocka(float x, float y){};
+	Tocka(const Tocka &T){};
+};
+
+void main(){
+	
+}
