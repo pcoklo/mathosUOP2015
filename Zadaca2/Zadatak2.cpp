df69d7f4d633b2c9bf2fad60814a91e13ef2bdc1
pcoklo <petarcoklo@gmail.com>
2015-04-04 17:31:46 +0200

    malo radi
    konstruktori i kvazi push i pop

--- Zadaca2/Zadatak2.cpp
+++ Zadaca2/Zadatak2.cpp
@@ -1,26 +1,58 @@
 #include <iostream>
 using namespace std;
 
-class vector
-{
+class vector{
 protected:
 	size_t s;
 	size_t c;
+
 	float *vektor;
 
 public:
-	vector(){};
-	vector(size_t size);
-	size_t size() const; //size_t = unsigned int
-	size_t capacity() const;
-	// dodavanje i brisanje elemenata s kraja
-	void push_back(float x);
-	float pop_back();
-	void resize_to_fit();
-	void clear();
+	vector(){
+		s = 0;
+		c = 4;
+		vektor = new float [c];
+	};
+
+	vector(size_t size){
+		s = 0;
+		c = size;
+		vektor = new float [c];	
+	};
+
+	vector(const vector& V){
+		c = V.c;
+		s = V.s;
+		vektor = new float [c];
+		for(int i=0; i<s; i++)
+			vektor[i]=V.vektor[i];
+	};
+
+	~vector(){ delete[] vektor; };
+
+	size_t size() const{}; //size_t = unsigned int
+	size_t capacity() const{};
+
+	void push_back(float x){
+		vektor[s]=x;
+		s++;	
+	};
+
+	float pop_back(){
+		s--;	
+		return vektor[s];	
+	};
+
+	void resize_to_fit(){};
+	void clear(){};
 };
 
 int main(){
-
-	return 1;
+	vector S1;
+	S1.push_back(21);
+	S1.push_back(11);
+	cout << S1.pop_back() << endl;
+	cout << S1.pop_back() << endl;
+	return 0;
 }
