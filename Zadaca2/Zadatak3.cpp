bd34ed044cb64f785dfec349ec51b34c19649e34
pcoklo <petarcoklo@gmail.com>
2015-04-04 15:41:07 +0200

    prva tocka

--- Zadaca2/Zadatak3.cpp
+++ Zadaca2/Zadatak3.cpp
@@ -6,11 +6,13 @@ class Tocka
 public:
 	float x;
 	float y;
-	Tocka(){};
-	Tocka(float x, float y){};
-	Tocka(const Tocka &T){};
+	Tocka(): x(0), y(0) {};
+	Tocka(float x, float y): x(x), y(y) {};
+	Tocka(const Tocka &T): x(T.x), y(T.y) {};
 };
 
-void main(){
-	
+int main(){
+	Tocka A(0,2);
+	cout << A.x << endl << A.y << endl;
+	return 1;
 }
