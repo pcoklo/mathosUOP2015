655e6257d3d43a8f7d683397a4434c3fdf26d5e9
pcoklo <petarcoklo@gmail.com>
2015-04-04 19:21:19 +0200

    zadatak 3 pravokutnik radi
    svi skonstruktori i sve metode rade ko podmazane,

--- Zadaca2/Zadatak3.cpp
+++ Zadaca2/Zadatak3.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 class Tocka
@@ -11,8 +12,66 @@ public:
 	Tocka(const Tocka &T): x(T.x), y(T.y) {};
 };
 
-int main(){
-	Tocka A(0,2);
-	cout << A.x << endl << A.y << endl;
-	return 1;
+class Pravokutnik
+{
+public:
+	Tocka T1,T2;
+
+	Pravokutnik();
+	Pravokutnik(const Tocka& T1, const Tocka& T2);
+	Pravokutnik(const Pravokutnik& P);
+	// metode za racunanje
+	float opseg() const;
+	float povrsina() const;
+};
+
+class PravilniPoligon
+{
+public:
+	Tocka S, T1;
+	int n;
+	Tocka *vrhovi;
+
+	PravilniPoligon();
+	PravilniPoligon(const Tocka& S, const Tocka& T, int n);
+	PravilniPoligon(const PravilniPoligon& P);
+
+	float opseg() const;
+	float povrsina() const;
+};
+
+Pravokutnik::Pravokutnik(){
+	T1.x=0; T1.y=0;
+	T2.x=0; T2.y=0;
+}
+
+Pravokutnik::Pravokutnik(const Tocka& T1, const Tocka& T2){
+	this->T1.x = T1.x; this->T1.y = T1.y;
+	this->T2.x = T2.x; this->T2.y = T2.y;
+}
+
+Pravokutnik::Pravokutnik(const Pravokutnik& P){
+	this->T1.x=P.T1.x; this->T1.y=P.T1.y;
+	this->T2.x=P.T2.x; this->T2.y=P.T2.y;
+}
+
+float Pravokutnik::opseg() const{
+	float a = abs(T1.x-T2.x);
+	float b = abs(T1.y-T2.y);
+	return 2*a + 2*b;
+}
+
+float Pravokutnik::povrsina() const{
+	float a = abs(T1.x-T2.x);
+	float b = abs(T1.y-T2.y);
+	return a*b;
+}
+
+int main()
+{
+	Pravokutnik P(Tocka(1,3), Tocka(2,5));
+	cout << "***P: opseg:" << P.opseg() << ", povrsina: " << P.povrsina() << endl;
+//	PravilniPoligon P_5(Tocka(1,5), Tocka(3,6), 5);
+//	cout << "***P_5: opseg:" << P_5.opseg() << ", povrsina: " << P_5.povrsina() << endl;
+
 }
