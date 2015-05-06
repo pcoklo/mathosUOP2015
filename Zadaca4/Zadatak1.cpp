dd69a9228c2c800f56d7cef3a50e82a415894ed4
pcoklo <petarcoklo@gmail.com>
2015-05-06 11:54:54 +0200

    nemojte kopirati main

--- Zadaca4/Zadatak1.cpp
+++ Zadaca4/Zadatak1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 class Vektor {
@@ -42,53 +43,71 @@ public:
     ~Vektor(){delete [] P;}
 };
 
+//konstruktor stvara Vektor od velićine
 Vektor::Vektor(int s){
     len = s;
     P = new int [len];
     curr_len = 0;
 }
 
+//kopy konstruktor
 Vektor::Vektor(const Vektor &V){
-    if (V.curr_len){
+//  if(V.curr_len > 0){
+//  if(V.curr_len != 0){
+//  if(V.curr_len >= 1){
+    if(V.curr_len){     //ako postoji polje
+        //trenutna veličina i ukupna veličina starog objekta = trenutna i ukupna ovog objekta
         this -> curr_len = V.curr_len;
         this -> len = V.len;
-
+        //dinamička alokacija polja P
         this -> P = new int [this -> len];
-
+        //kopiraj svaki element polja iz jednog objekta u drugi
         for (int i = 0; i < this -> curr_len; i++)
             this -> P[i] = V.P[i]; 
     }
 }
 
+//dodaje element na prvo slobodno mjesto
 bool Vektor::append(int x){
+    //ako ima mjesta u polju
     if (curr_len < len){
-        P[curr_len] = x;
-        curr_len++;
-        return true;
+        P[curr_len] = x;    //stavi element na zadnje mjesto
+        curr_len++;         //pomakni to mjesto za jedan
+        return true;        //istina
     }
 
-    return false;
+    return false;           //nije istina
 }
 
 bool Vektor::search(int x){
-    if(curr_len)
-        for(int i=0; i<curr_len; i++)
-            if(P[i] == x) return true;
-    return false;
+//  if(V.curr_len > 0){
+//  if(V.curr_len != 0){
+//  if(V.curr_len >= 1){
+    if(curr_len)    //ako postoji polje
+        for(int i=0; i<curr_len; i++)   //prođi kroz sve elemente
+            if(P[i] == x) return true;  //ako si naišao na element, return true
+
+    //ako polje nepostoji i nisi našao element
+    return false;   //vrati false
 }
 
+
 void Vektor::ispisi(){
-    if (curr_len){
-        for (int i=0; i < curr_len; i++)
-            cout << P[i] << "\t";
-        cout << endl;
+//  if(V.curr_len > 0){
+//  if(V.curr_len != 0){
+//  if(V.curr_len >= 1){
+    if (curr_len){  //ako postoji polje
+        for(int i=0; i<curr_len; i++)   //prođi kroz sve elemente
+            cout << P[i] << "\t";       //i ispiši svaki i dodaj tabulator
+        cout << endl;   //dodaj endl na kraju
     }
 }
 
 void Vektor::sort(){
-    if(curr_len){
+//  if(V.curr_len >= 2){
+    if(curr_len > 1){   //ako postoji polje i veće je od 1
         bool sorted = false;
-        while (!sorted){
+        while (!sorted){    //sortiraj bablsortom
             sorted = true;
             for(int i=0; i<curr_len - 1; i++){
                 if(P[i]>P[i+1]){
@@ -102,39 +121,63 @@ void Vektor::sort(){
     }
 }
 
+//množenje vektora je suma umnožaka i-tih elemenata jednog i drugog objekta
 void Vektor::Mnozi(const Vektor &V){
     //ako su trenutne duljine vektora jednake
     if (this -> curr_len == V.curr_len){
-        int sum=0;
+        int sum=0;  //suma je = 0 jepribrojavamo u nju umnoške elemenata
+
         for (int i=0; i<V.curr_len; i++)
-            sum += this -> P[i] * V.P[i];
-        cout << "Skalarni produkt = " << sum << endl;           
+            sum += this -> P[i] * V.P[i];   //pomnoži i zbrajaj u sumu
+        //  sum = sum + this -> P[i] * V.P[i];
+
+        cout << "Skalarni produkt = " << sum << endl;
+    //  coit << sum << endl;
+    //  cout << "Umnožak vektora = " << sum << endl;
     }
     else cout << "neože se množit" << endl;
 }
 
+//ako zovemo funkciju vektora V od vektora V1 i V2 -> V=V1+V2
+//npr V.Zbroji(V1,V2); V će biti zbroj ta dva vektora
+// this je V
 void Vektor::Zbroji(const Vektor &V1, const Vektor &V2){
+    //ako su tranutne duljine vektora jednake
     if(V1.curr_len == V2.curr_len){
         this -> len = V1.len;
+    //  this -> len = V2.len;
+
         this -> curr_len = V1.curr_len;
+    //  this -> curr_len = V2.curr_len;
+
+        //diniamički alociraj polje vektora veličine ukupne duljine
         this -> P = new int [this -> len];
 
         for (int i=0; i<V1.curr_len; i++)
             this -> P[i] = V1.P[i] + V2.P[i];
+            //zbroji sve element i stavi ih u polje novog objekta
     }
 }
 
+//desni kopiramo u lijevi
 void Vektor::operator=(const Vektor &V){
-    if(V.curr_len){
+//  if(V.curr_len > 0){
+//  if(V.curr_len != 0){
+//  if(V.curr_len >= 1){
+    if(V.curr_len){     //ako postoji vektor
+        //obriši staro polje, ako postoji
         if (this -> P) delete [] this -> P;
 
+        //kopiraj vrijednosti
         this -> curr_len = V.curr_len;
         this -> len = V.len;
 
+        //stvori novo polje
         this -> P = new int [this -> len];
 
         for (int i=0; i < this -> curr_len; i++)
             this -> P[i] = V.P[i];
+            //kopiraj elemente iz jednog u drugi
     }
 }
 
@@ -152,13 +195,19 @@ void Vektor::operator-=(const Vektor &V){
     }
 }
 
+//Z = V1 + V2
+//this = V1; V= V2
 Vektor Vektor::operator+(const Vektor &V) const{
     if(this -> curr_len == V.curr_len){
+        //stvori novi objekt kao kopiju prosljedenog objekta
         Vektor Z(V);
+        //Z=V2
 
+        //Z+=V1
         for(int i=0; i<Z.curr_len; i++)
             Z.P[i] += this -> P[i];
 
+        //Z = V1 + V2;
         return Z;
     }
 }
@@ -185,12 +234,18 @@ int Vektor::operator*(const Vektor &V) const{
     }
 }
 
+//vrati jednako ako je svaki element jednak svakom elementu
+//ako su ukupne i trenutne duljine jednake
+//uprotivnom vrati false
 bool Vektor::operator==(const Vektor &V) const{
     if(this -> curr_len == V.curr_len && this -> len == V.len){
         for (int i=0; i<V.curr_len; i++)
             if(this -> P[i] != V.P[i]) return false;
+            //ako je bilo koji element razlićit return false
+        //ako nisi napravio return dosad, znači da su svi elementi jednaki
         return true;
     }
+//ako if nije sitina return false
     return false;
 }
 
@@ -198,26 +253,41 @@ int & Vektor::operator[](int i) const{
     return this -> P[i];
 }
 
-ostream& operator<<(ostream& buffer, const Vektor &V){
-    for(int i = 0; i<V.curr_len; i++)
-        buffer << V.P[i] << "\t";
-    buffer << endl;
-    return buffer;
-}
 
 int main(){
     Vektor V1(6), V2(6);
     V1.append(7); V1.append(6); V1.append(11);
     V2.append(16);V2.append(8); V2.append(4);
 
-    cout << V1 << V2;
+    if(V1==V2) cout << "V1 == V2" << endl;
+    else cout << "V1 != V2" << endl;
+
     Vektor C(6);
 
     C = V1+V2;
 
-    cout << C << endl;
+    C.ispisi();
 
     V1.Mnozi(V2);
-    
+
+    V1 = V2;
+
+    if(V1==V2) cout << "V1 == V2" << endl;
+    else cout << "V1 != V2" << endl;
+
+    int n=30;
+    Vektor V(n);
+    Vektor H(n);
+    for(int i=0; i<n; i++){
+        V.append(rand()%10);
+        H.append(rand()%10);
+    }
+    V.ispisi();
+    H.ispisi();
+    V.sort(); V.ispisi();
+    H.sort(); H.ispisi();
+    H+=V;
+    H.ispisi();
+
     return 0;
 }
